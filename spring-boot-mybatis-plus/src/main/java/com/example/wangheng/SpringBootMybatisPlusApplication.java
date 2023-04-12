db6a166784bceb5b6de01e0a30d03ac391a087dd
wangheng <2812490576@qq.com>
2023-04-12 21:13:58 +0800

    wangheng first commit

---
+++ spring-boot-mybatis-plus/src/main/java/com/example/wangheng/SpringBootMybatisPlusApplication.java
@@ -0,0 +1,16 @@
+package com.example.wangheng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 28124
+ */
+@SpringBootApplication
+public class SpringBootMybatisPlusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMybatisPlusApplication.class, args);
+    }
+
+}
